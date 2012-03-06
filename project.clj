ae29459fc7f23577548d808889e9ec06024af76b
Arthur Edelstein <arthuredelstein@gmail.com>
2012-03-06 00:12:09 -0800

    include data.json and bump version

--- project.clj
+++ project.clj
@@ -1,4 +1,5 @@
-(defproject solrclient "0.0.1"
+(defproject solrclient "0.0.2"
   :description "a clojure client for solr servers (using the json protocol)"
   :dependencies [[org.clojure/clojure "1.3.0"]
+                 [org.clojure/data.json "0.1.2"]
                  [clj-http "0.2.7"]])
