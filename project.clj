68b02af21b059c21d22c2f075724020f81185c5c
Arthur Edelstein <arthuredelstein@gmail.com>
2012-03-06 00:04:35 -0800

    create a description

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
 (defproject solrclient "0.0.1"
-  :description "FIXME: write"
+  :description "a clojure client for solr servers (using the json protocol)"
   :dependencies [[org.clojure/clojure "1.3.0"]
                  [clj-http "0.2.7"]])
