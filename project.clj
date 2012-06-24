d877ba78fa296dc4a3c2da7fdec97f2e089e542f
Arthur Edelstein <arthuredelstein@gmail.com>
2012-06-23 21:32:55 -0700

    upgrade to clojure 1.4.0; bump version

--- project.clj
+++ project.clj
@@ -1,5 +1,5 @@
-(defproject solrclient "0.0.2"
+(defproject solrclient "0.0.3"
   :description "a clojure client for solr servers (using the json protocol)"
-  :dependencies [[org.clojure/clojure "1.3.0"]
-                 [org.clojure/data.json "0.1.2"]
-                 [clj-http "0.2.7"]])
+  :dependencies [[org.clojure/clojure "1.4.0"]
+                 [org.clojure/data.json "0.1.3"]
+                 [clj-http "0.4.3"]])
